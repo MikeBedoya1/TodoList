512059553de7e9c1811b70b4fb20042147e5012f
Mike Bedoya <mikebedoya28@gmail.com>
2024-09-09 17:35:46 +0000

    First Commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("")
+	const [exercises, setExercises] = useState ([])
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="container">
+			
+			<h1 color="green">GORILLA RAW EXERCISES</h1>
+			<ul d-flex justify-content-end>
+				<li><input type="text"
+					onChange={(event) => setInputValue(event.target.value)}
+					value={inputValue}
+					onKeyUp={(event) => {
+						if (event.key === "Enter") {
+							console.log(inputValue);
+							
+							setExercises([... exercises, inputValue]);
+							setInputValue("")
+						}
+					}
+					}
+					placeholder="what exercises do you gonna do?" />
+
+				</li>
+
+				{exercises.map((item, index) => (
+					<li>
+						{item} {""} <i class="fas fa-trash" onClick={() => setExercises(exercises.filter((t, currentIndex) => index != currentIndex
+
+						))}
+						></i>
+					</li>
+
+				))}
+			</ul>
+			<div>{exercises.length} tasks</div>
+			<div>
+
+			<img src="https://as2.ftcdn.net/v2/jpg/05/58/97/37/1000_F_558973789_iGbbV5rUJDObkkqHVyFnypNDkYrUjanD.jpg" alt="" />
+			</div>
+
+		</div >
 	);
 };
 
